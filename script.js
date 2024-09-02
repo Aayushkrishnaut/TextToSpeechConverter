e6dd533a64b11a607807fc70c370c3a884000a27
Aayushkumar Yadav <140250471+Aayushkrishnaut@users.noreply.github.com>
2024-09-02 21:57:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+  voices = window.speechSynthesis.getVoices();
+  speech.voice = voices[0];
+
+  voices.forEach((voice, i) => {
+    let option = new Option(voice.name, i);  // Corrected 'new option' to 'new Option'
+    voiceSelect.options.add(option);
+  });
+};
+
+voiceSelect.addEventListener("change", () => {
+  speech.voice = voices[voiceSelect.value];
+});
+
+document.querySelector("button").addEventListener("click", () => {
+  speech.text = document.querySelector("textarea").value;
+  window.speechSynthesis.speak(speech);
+});
